aa7bc66a7ce33364d83b17fef1d3454740962548
Kelebrimbor <egor_anikin_n@mail.ru>
2020-05-16 16:37:10 +0300

    LoginFormTest1

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,28 +6,34 @@ class LoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'введите логин'
+            value: 'введите логин',
+            value1:'введите пароль'
         };
         this.click = this.click.bind(this);
         this._subal=this._subal.bind(this);
     }
     click(){
-        this.setState({value: event.target.value});}
-    _subal(event) {
-        alert('Сочинение отправлено: ' + this.state.value);
-        event.preventDefault();
+        this.setState(
+            {value: event.target.value});}
+
+_subal(event) {
+        alert('вход выполнен, : ' + this.state.value + '   подождите.'+ ' пароль '+ this.state.value1);
+    event.preventDefault();
     }
 
     render() {
         return (
-            <form onSubmit={this.click}>
+            <form onSubmit={this._subal}>
                 <label>
-                    Сочинение:
-                    <textarea value={this.state.value} onChange={this._subal} />        </label>
+                    логин:
+                    <textarea value={this.state.value} onChange={this.click} />        </label>
+                <label>
+                    пароль:
+                    <textarea value={this.state.value1}  />        </label>
                 <input type="submit" value="Отправить" />
             </form>
         );
     }
 }
-export default index;
+export default LoginForm;
 
