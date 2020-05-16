83bfd693f9ed59d7281baad5cdd020fae977d431
Kelebrimbor <egor_anikin_n@mail.ru>
2020-05-16 17:50:02 +0300

    LoginFormTest2

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,34 +6,42 @@ class LoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 'введите логин',
-            value1:'введите пароль'
+            login: 'введите логин',
+            password: 'введите пароль'
         };
         this.click = this.click.bind(this);
-        this._subal=this._subal.bind(this);
+        this.click1=this.click1.bind(this);
+        this.submit = this.submit.bind(this);
     }
-    click(){
-        this.setState(
-            {value: event.target.value});}
 
-_subal(event) {
-        alert('вход выполнен, : ' + this.state.value + '   подождите.'+ ' пароль '+ this.state.value1);
-    event.preventDefault();
+    click() {
+        this.setState(
+            {login: event.target.login});
+    }
+    click1() {
+        this.setState(
+            {password: event.target.password});
+    }
+
+    submit(event) {
+        alert('вход выполнен, : ' + this.state.login + '   подождите.' + ' пароль ' + this.state.password);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <form onSubmit={this._subal}>
+            <form onSubmit={this.submit}>
                 <label>
                     логин:
-                    <textarea value={this.state.value} onChange={this.click} />        </label>
+                    <textarea value={this.state.login} onChange={this.click}/> </label>
                 <label>
                     пароль:
-                    <textarea value={this.state.value1}  />        </label>
-                <input type="submit" value="Отправить" />
+                    <textarea value={this.state.password} onChange={this.click1}/> </label>
+                <input type="submit" value="Отправить"/>
             </form>
         );
     }
 }
+
 export default LoginForm;
 
